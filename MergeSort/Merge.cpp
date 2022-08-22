cb1e97872dc976512df0c0701ea5043c0a248771
Tawsif F Rahman <rahmantawsif@gmail.com>
2022-08-22 12:45:29 +0600

    Merge done

---
+++ MergeSort/Merge.cpp
@@ -0,0 +1,41 @@
+#include "Merge.h"
+
+std::vector<int> Merge::twoWay(std::vector<int>& list1 , std::vector<int>& list2){
+    int i = 0;
+    int j = 0;
+    std::vector<int> result ;
+    int elem;
+
+    // Comparision push
+    while(i<list1.size() && j<list2.size()){ 
+        if(list1[i] < list2[j]){
+            elem = list1[i];
+            i++;
+        }else{
+            elem = list2[j];
+            j++;
+        }
+        result.push_back(elem);
+    }
+    
+    // Remaining push
+    for(;i<list1.size();i++){ 
+        result.push_back(list1[i]);
+    }
+    for(;j<list2.size();j++){
+        result.push_back(list2[j]);
+    }
+    return result;
+}
+
+
+int main(){
+    Merge m;
+    std::vector <int> l1 = {2,5,8,12,45};
+    std::vector <int> l2 = {1,3,4,8,34,67,89};
+    std::vector <int> l3 = m.twoWay(l1, l2);
+    for(auto e:l3){
+        std::cout<< e << "\t";
+    }
+    return 0;
+}
---
+++ MergeSort/Merge.h
@@ -0,0 +1,9 @@
+#ifndef MERGE_H
+#define MERGE_H
+#include <iostream>
+#include <vector>
+class Merge{
+    public:
+        std::vector<int> twoWay(std::vector<int>&, std::vector<int>&);
+};
+#endif
