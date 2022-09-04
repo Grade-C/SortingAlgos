c997e53ff7300ea6ba56032ddeeeeb477b7788ab
Tawsif F Rahman <rahmantawsif@gmail.com>
2022-09-04 21:10:53 +0600

    Code formatting

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -32,6 +32,9 @@ void QuickSort::sort(int low, int high, std::vector<int> &list, Order order)
 void QuickSort::sort(std::vector<int> &input, Order order)
 {
     modifyList(input, order);
+    //Utils::printVector(input);
+    //return;
+
     std::cout << "Modded : ";
     Utils::printVector(input);
     sort(0, input.size() - 1, input, order);
@@ -47,8 +50,9 @@ int main()
 {
 
     QuickSort qs;
-    std::vector<int> unsortedData = {34,1,1,13, 10, 11, -9, 12, 4, 1,-7, -9 , 139, 139, 45,45, 45, 25, 12,   7, 56, 6, 8, 45, 10 , 45 ,34, 10, 12, 3, 11, 45,-7};
+    //std::vector<int> unsortedData = {34,1,1,13, 10, 11, -9, 12, 4, 1,-7, -9 , 139, 139, 45,45, 45, 25, 12,   7, 56, 6, 8, 45, 10 , 45 ,34, 10, 12, 3, 11, 45,-7};
+    std::vector<int> unsortedData = {13, 10, 11, 4, 139, 25, 7, 56, 6, 8, 12, 3};
     Utils::printVector(unsortedData);
-    qs.sort(unsortedData, qs.Asc);
+    qs.sort(unsortedData, qs.Des);
     return 0;
 }
